e1e71c1e80d994aa6781fb86eb73dfed64f42f59
Jay Zelos <jay@zelos.org.uk>
2020-02-25 21:56:34 +0000

    added close to toast

--- UI/ClientApp/src/Components/Toasts/ErrorToast.tsx
+++ UI/ClientApp/src/Components/Toasts/ErrorToast.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Toast from 'react-bootstrap/Toast';
 
 interface Props {
@@ -7,7 +7,13 @@ interface Props {
 }
 
 const ErrorToast: React.FunctionComponent<Props> = (props) => {
-    return <Toast
+
+    const [show, setShow] = useState(true);
+    const toggleShow = () => setShow(!show);
+
+    return <Toast 
+        show={show} 
+        onClose={toggleShow}
         style={{
             position: 'absolute',
             top: 0,
@@ -16,10 +22,9 @@ const ErrorToast: React.FunctionComponent<Props> = (props) => {
     >
         <Toast.Header>
             <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-            <strong className="mr-auto">Error!</strong>
-            <small>props.header</small>
+            <strong className="mr-auto">{props.header}</strong>            
         </Toast.Header>
-        <Toast.Body>props.body</Toast.Body>
+        <Toast.Body>{props.body}</Toast.Body>
     </Toast>
 }
 
